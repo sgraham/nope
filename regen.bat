78fde2b9961efc3c89dd9526a8b9222eae0aa379
Scott Graham <scottmg@chromium.org>
2015-03-17 00:17:45 -0700

    debug shared, release static

--- regen.bat
+++ regen.bat
@@ -2,4 +2,7 @@
 call python build\vs_toolchain.py update
 call python build\util\lastchange.py -o build\util\LASTCHANGE
 call python build\util\lastchange.py -s third_party/WebKit -o build\util\LASTCHANGE.blink
-call python build\gyp_chromium
+call python build\gyp_chromium -Gconfig=Debug -Dcomponent=shared_library
+call python build\gyp_chromium -Gconfig=Release
+echo Debug is shared_library
+echo Release is static_library
